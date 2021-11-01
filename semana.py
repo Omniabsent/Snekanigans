1be7986358d407e1a27b8b8e15c6231de40ff702
They call me JÃºlia <90799317+Omniabsent@users.noreply.github.com>
2021-11-01 09:55:46 -0300

    Descobre em que dia da semana cai uma data
    Versão 2.0, agora funciona em qualquer ano do século 21

--- semana.py
+++ semana.py
@@ -1,5 +1,3 @@
-#só sabe dizer o dia da semana que cai a data em 2021 porque eu não entendi como calcular a chave do ano
-
 print("Insira a data por partes")
 dia = int(input("Dia:"))
 mes = int(input("Mês:"))
@@ -8,7 +6,22 @@ ano = int(input("Ano:"))
 chavesMes = {1: 1, 2: 4, 3: 4, 4: 0, 5: 2, 6: 5, 7: 0, 8: 3, 9: 6, 10: 1, 11: 4, 12: 6}
 valorMes = chavesMes.get(mes)
 
-valorSemana = (dia + valorMes + 4)%7  #4 é a chave de 2021
+valorAno = 4
+for i in range(ano+1): 
+    if valorAno < 6:
+        valorAno +=1
+    else: 
+        valorAno = 0
+    if (i%400 == 0) or (i%4 == 0 and not(i%100 == 0 and i%400 != 0)):
+        if valorAno < 6:
+            valorAno +=1
+        else: 
+            valorAno = 0
+
+if mes <= 2:
+    valorAno -= 1
+
+valorSemana = (dia + valorMes + valorAno)%7 
 
 nomeDia = {1: "Domingo", 2: "Segunda", 3: "Terça", 4: "Quarta", 5: "Quinta", 6: "Sexta", 0: "Sábado"}
 resultado = nomeDia.get(valorSemana)
